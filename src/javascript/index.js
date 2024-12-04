afbe7ddcbfa2d8e4b9485b0c75bc45e699acd862
curso06 <curso06@gmail.com>
2024-12-04 13:56:20 +0100

    Ejemplo de javascript

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,27 @@
-console.log('Hola mundo');
+
+const span = document.querySelector('div span');
+
+document.querySelector('button').addEventListener('click', function() {
+    span.textContent = 'Has pulsado el boton 1'; 
+    span.style.backgroundColor = 'gold'; 
+    span.style.padding = '10px';
+    span.style.border = '2px solid black';
+});
+
+document.querySelector('.botonUno').addEventListener('click', function() {
+    span.textContent = 'Has pulsado el boton 1'; 
+    span.style.color = 'red'; 
+});
+
+document.querySelector('.botonDos').addEventListener('click', function() {
+    span.textContent = 'Has pulsado el boton 2'; 
+    span.style.color = 'blue'; 
+});
+
+document.querySelector('.botonBorrar').addEventListener('click', function() {
+    span.textContent = ''; 
+    span.style.padding = '';
+    span.style.border = '';
+});
+
+console.log('Hola mundo otra vez');
