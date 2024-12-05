350cc805cd0f7797130a86855f068493aa1d6858
curso06 <curso06@gmail.com>
2024-12-05 13:59:55 +0100

    show y hide

--- src/javascript/index2.js
+++ src/javascript/index2.js
@@ -1,18 +1,44 @@
 
 $(function() {
 
-    solicitud = {
+    solicitudes = [{
         "id": 1,
         "nombre" : "Juan",
         "apellido" : "Secreto",
-    }
+    },
+    {
+        "id": 2,
+        "nombre" : "Antonio",
+        "apellido" : "Mesa",
+    },
 
-    solicitudes = [solicitud, solicitud, solicitud];
+    {
+        "id": 3,
+        "nombre" : "Jaime",
+        "apellido" : "Trueba",
+    }];
 
     for (let i = 0; i < solicitudes.length; i++) {
         $('#maestro').append(
             $('<li>').text(solicitudes[i].nombre + ' ' + solicitudes[i].apellido)
+            .attr('id' , 'id' + solicitudes[i].id)
         );
     }
+
+
+    $('li').on('click', function(event) {
+        if($('#detalle').is(':visible')){
+            $('#detalle').hide();
+        } else {
+            $('#detalle').show();
+
+            let solicitud= $(this).attr('id');
+
+            $('#id').val(solicitud);
+            $('#nombre').val(solicitud.nombre);
+            $('#apellido').val(solicitud.apellido);
+        }
+        
+    });
 });
 
